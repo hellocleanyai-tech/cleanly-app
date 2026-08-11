f10fa59ede4c686dbd58e20786b1c96877ec960a
EmmanuelNyembueAlexandre <168066883+EmmanuelNyembueAlexandre@users.noreply.github.com>
2026-08-11 04:40:26 +0200

    Update LS_STARTER_TRIAL_URL in env.js

--- cleanly-app/public/assets/js/env.js
+++ cleanly-app/public/assets/js/env.js
@@ -9,7 +9,7 @@ window.__CLEANLY_ENV__ = {
   "SUPABASE_URL": "https://hupeqzyrzrtwymdoogbn.supabase.co",
   "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1cGVxenlyenJ0d3ltZG9vZ2JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NjIzNjEsImV4cCI6MjA4NjMzODM2MX0.b35ZtHuZ-Rqdm4eSwt1a4mgzfmqErXXC5_y7pBIwZeE",
   "LS_CHECKOUT_URL": "https://cleanly-app.lemonsqueezy.com/checkout",
-  "LS_STARTER_TRIAL_URL": "https://cleanly-app.lemonsqueezy.com/checkout/buy/951fa1d7-06ec-400a-b43e-fb3f0331d49a",
+  "LS_STARTER_TRIAL_URL": "https://cleanly-app.lemonsqueezy.com/checkout/buy/cf8736bf-e22c-4b68-95b0-c1dec48293ec",
   "LS_STARTER_STANDARD_URL": "https://cleanly-app.lemonsqueezy.com/checkout/buy/b21dd36e-37ac-4a7a-86f1-88a57b94ec6d",
   "SUPPORT_EMAIL": "support@cleanly.ai"
 };
